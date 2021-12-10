8239f91546bcd722c52b9e5977927fd7b6faa279
H-H Traders <alikamran07@gmail.com>
2021-12-10 14:45:20 +0500

    new update

--- app/src/main/java/bhailog/kami/myfinaltoaster/MainActivity.java
+++ app/src/main/java/bhailog/kami/myfinaltoaster/MainActivity.java
@@ -3,12 +3,42 @@ package bhailog.kami.myfinaltoaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import bhailog.kami.my_final_toaster.MyToaster;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText val1,val2;
+    TextView tv_Result;
+    Button get_result;
+    String a,b;
+    double save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getting values
+        val1=findViewById(R.id.val1);
+        val2=findViewById(R.id.val2);
+
+        tv_Result=findViewById(R.id.result_show);
+        get_result=findViewById(R.id.result_btn);
+
+        get_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a= val1.getText().toString();
+                b=val2.getText().toString();
+                save=MyToaster.sum(Double.parseDouble(a),Double.parseDouble(b));
+
+                tv_Result.setText(""+save);
+            }
+        });
+
     }
 }
--- my-final-toaster/src/main/java/bhailog/kami/my_final_toaster/MyToaster.java
+++ my-final-toaster/src/main/java/bhailog/kami/my_final_toaster/MyToaster.java
@@ -1,9 +1,15 @@
 package bhailog.kami.my_final_toaster;
 
 import android.content.Context;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MyToaster {
+    public static double sum(double val, double val2)
+    {
+        return val+val2;
+    }
+
     public static void s(Context context,String msg)
     {
         Toast.makeText(context,msg, Toast.LENGTH_SHORT).show();
